d2df6c0d982a02356e6b14a7df04deb559ac2622
Haoxiang310 <135764868+Haoxiang310@users.noreply.github.com>
2023-10-23 19:28:50 -0700

    [math]:equation judge during collision with platform (#6)
    Co-authored-by: meivenka <ln.meivenkatkumar21@gmail.com>

--- Assets/GeneralScripts/PlayerControl.cs
+++ Assets/GeneralScripts/PlayerControl.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using TMPro;
 using UnityEngine;
+using TMPro;
 
 public class PlayerControl : MonoBehaviour
 {
@@ -32,6 +33,60 @@ public class PlayerControl : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D obstacle)
     {
+        if (obstacle.gameObject.CompareTag("Ground"))
+            isGrounded = true;
+
+        // 如果玩家与一个障碍物碰撞
+        if (obstacle.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("We're here!");
+            // foreach (Transform child in obstacle.gameObject.transform)
+            // {
+            //     Component[] components = child.GetComponents<Component>();
+            //     foreach (Component component in components)
+            //     {
+            //         Debug.Log("Child: " + child.name + ", Component Type: " + component.GetType().ToString());
+            //     }
+            // }
+
+            TextMeshPro obstacleEquationText = obstacle.gameObject.GetComponentInChildren<TextMeshPro>();
+            if (obstacleEquationText != null)
+            {
+                // Debug.Log("Found TextMeshPro with text: " + obstacleEquationText.text);
+            }
+            else
+            {
+                // Debug.Log("TextMeshPro not found!");
+            }
+
+            if (obstacleEquationText != null)
+            {
+                GameObject obj = GameObject.Find("EquationManager");
+                
+                JudgeEquation judge = obj.GetComponent<JudgeEquation>();
+
+                // 设置JudgeEquation组件的equationText属性为障碍物上的方程
+                judge.equationText = obstacleEquationText;
+
+                // 设置JudgeEquation的targetObject为碰撞的障碍物
+                judge.targetObject = obstacle.gameObject;
+
+                // 获取玩家上的TextMeshProUGUI组件的值
+                GameObject player = GameObject.Find("Player");
+                judge.playerEquationText = player.GetComponentInChildren<TextMeshPro>();
+                Debug.Log(judge.playerEquationText.text);
+                Debug.Log(judge.equationText.text);
+
+
+                if (judge.playerEquationText != null)
+                {
+                    judge.varValue = int.Parse(judge.playerEquationText.text);
+                }
+                
+                // 调用EvaluateFromTextMeshPro方法来检查玩家的数值是否满足方程
+                judge.EvaluateFromTextMeshPro();
+            }
+
         // Jump Enabled
         if(obstacle.gameObject.CompareTag("Ground"))
             isGrounded = true;
@@ -64,6 +119,7 @@ public class PlayerControl : MonoBehaviour
         // Jump Disabled
         if (obstacle.gameObject.CompareTag("Ground"))
             isGrounded = false;
+        
     }
 
     // todo: when grab worm and pebble, make them use gravity. // currently, they are kinematic and don't use gravity.
