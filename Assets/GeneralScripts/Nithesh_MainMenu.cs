e68f4db717906638f9848d150a43596a90b1f1c2
nitheshreddy939 <50142613+nitheshreddy939@users.noreply.github.com>
2023-11-06 13:48:11 -0800

    Level Selection menu options

---
+++ Assets/GeneralScripts/Nithesh_MainMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nithesh_MainMenu: MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+   
+}
