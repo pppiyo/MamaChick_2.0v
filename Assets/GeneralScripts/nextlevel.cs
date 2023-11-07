315741206fc881e6382d0e62d1e457b5e812f740
nitheshreddy939 <50142613+nitheshreddy939@users.noreply.github.com>
2023-11-07 12:26:57 -0800

    LoadNextLevel Implementation

--- Assets/GeneralScripts/nextlevel.cs
+++ Assets/GeneralScripts/nextlevel.cs
@@ -1,10 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class nextlevel: MonoBehaviour
+public class NextLevel: MonoBehaviour
 {
-    public void LoadStartMenuScene()
+    public void LoadNextLevel()
     {
-        SceneManager.LoadScene("_Level2");
+        // Get the current scene index
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        // Check if the current scene index is valid
+        if (currentSceneIndex >= 0 && currentSceneIndex < SceneManager.sceneCountInBuildSettings - 1)
+        {
+            // Load the next scene by incrementing the current scene index
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+        else
+        {
+            // Handle the case when there are no more levels
+            Debug.Log("No more levels available.");
+        }
     }
 }
