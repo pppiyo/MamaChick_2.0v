96debceed0aa64ec8def5c50b6ec3d34b34c0049
meivenka <ln.meivenkatkumar21@gmail.com>
2023-11-13 01:42:52 -0800

    Revert "Build branch (#75)"
    This reverts commit aebaf770e22846d3cbc99d2fb4f2b205aaa682e1.

--- Assets/GeneralScripts/TutorialManager.cs
+++ Assets/GeneralScripts/TutorialManager.cs
@@ -168,12 +168,14 @@ public class TutorialManager : MonoBehaviour
         tutorialElements = GameObject.FindGameObjectsWithTag("Platform_Mutate");
         foreach (GameObject obj in tutorialElements)
         {
-            obj.SetActive(true);
+            if (GlobalVariables.curLevel != "tutorial 3" && GlobalVariables.curLevel != "tutorial 4" && GlobalVariables.curLevel != "tutorial 5")
+                obj.SetActive(false);
         }
         tutorialElements = GameObject.FindGameObjectsWithTag("Checkpoint");
         foreach (GameObject obj in tutorialElements)
         {
-            obj.SetActive(true);
+            if (GlobalVariables.curLevel != "tutorial 3" && GlobalVariables.curLevel != "tutorial 4" && GlobalVariables.curLevel != "tutorial 5")
+                obj.SetActive(false);
         }
         tutorialElements = GameObject.FindGameObjectsWithTag("Goal");
         foreach (GameObject obj in tutorialElements)
@@ -303,6 +305,7 @@ public class TutorialManager : MonoBehaviour
         Platform_1_8.SetActive(true);
         Platform_1_9.SetActive(true);
         Platform_1_10.SetActive(true);
+        Destination.SetActive(true);
         level1Stage4 = 5;
     }
 
